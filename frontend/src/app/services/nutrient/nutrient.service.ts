71b23de9d54149d79cb515886e4b7698c28c03d3
Grob Oliver <groboliver1117@gmail.com>
2018-11-28 09:48:49 +0100

    Nutrient service added

---
+++ frontend/src/app/services/nutrient/nutrient.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+
+import { Observable } from "rxjs/internal/Observable";
+import { catchError, tap } from 'rxjs/operators';
+import { of } from "rxjs/internal/observable/of";
+
+import { Nutrient } from "../../models/Nutrient";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NutrientService {
+
+  private baseNutritionUrl = 'http://localhost:60001/nutrition';
+
+  constructor(private http: HttpClient) { }
+
+  calculateNutrients(meal: string): Observable<Nutrient[]> {
+    return this.http.post<any>(`${this.baseNutritionUrl}`, {'meal': meal})
+      .pipe(
+        tap(_ => console.log(`Nutrients found`)),
+        catchError(response => this.handleError(response))
+      );
+  }
+
+  private handleError<T> (error: HttpErrorResponse, result?: T) {
+    console.error(error);
+    console.error(error.error['error']);
+    console.error(error.error['message']);
+    return of(result as T);
+  }
+
+}
