901f8ba6efc9575f850b55433e9ae7d381033d4e
anitaSzabo <azorka3@gmail.com>
2018-11-28 13:42:43 +0100

    main class renamed

--- workout-service/src/main/java/com/codecool/enterprise/workout/WorkoutApplication.java
+++ workout-service/src/main/java/com/codecool/enterprise/workout/WorkoutApplication.java
@@ -1,8 +1,7 @@
-package com.codecool.enterprise.avatar;
+package com.codecool.enterprise.workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorkoutApplication {
