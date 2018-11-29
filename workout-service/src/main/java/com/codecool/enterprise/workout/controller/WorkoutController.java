402df6e794a632a4b46bc88ec9373b43f4ec9bd1
anitaSzabo <azorka3@gmail.com>
2018-11-29 16:35:36 +0100

    Enums returned

--- workout-service/src/main/java/com/codecool/enterprise/workout/controller/WorkoutController.java
+++ workout-service/src/main/java/com/codecool/enterprise/workout/controller/WorkoutController.java
@@ -1,12 +1,18 @@
 package com.codecool.enterprise.workout.controller;
 
+import com.codecool.enterprise.workout.model.Exercise;
+import com.codecool.enterprise.workout.model.Training;
 import com.codecool.enterprise.workout.model.Workout;
 import com.codecool.enterprise.workout.service.WorkoutService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WorkoutController {
@@ -18,7 +24,16 @@ public class WorkoutController {
     @GetMapping("/workout")
     public List<Workout> getWorkouts() {
         List<Workout> workouts = service.getAllWorkouts();
-        System.out.println(workouts.get(0).getName());
         return workouts;
+    }
+
+    @PostMapping("/workout")
+    public void saveWorkout(@RequestBody String jsonRequest) {
+//        service.saveWorkout(jsonRequest);
+    }
+
+    @GetMapping("workout/exercises")
+    public Exercise[] getExercises() {
+        return Exercise.values();
     }
 }
